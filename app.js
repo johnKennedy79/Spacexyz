ea8d19d1dac454aab7abd261e26ef93ecdcba399
John Kennedy <john,m,r,kennedy.jk@gmail.com>
2024-07-06 14:08:07 +0100

    final draft

--- app.js
+++ app.js
@@ -25,32 +25,3 @@ function toggletran() {
 }
 
 btn2.addEventListener("click", toggletran);
-
-//button 3
-const btn3 = document.getElementById("visa");
-let z = document.querySelector(".app");
-
-function togglevisa() {
-  if (z.style.height === "0vh") {
-    z.style.height = "100vh";
-  } else {
-    z.style.height = "0vh";
-  }
-}
-
-btn3.addEventListener("click", togglevisa);
-
-//form selector and basket
-let Mercury = 1.446;
-let Venus = 1.736;
-let Mars = 2.671;
-let cost = 0;
-
-const p1 = document.getElementsByName("Mercury");
-const p2 = document.getElementsByName("Venus");
-const p3 = document.getElementsByName("Mars");
-const pr = document.getElementById("cost");
-
-function basket() {}
-
-document.addEventListener("change", basket);
